b87bb5bee9826e45cc5bd0a41799bc2f23e79042
cw-matsuba <matsuba@chatwork.com>
2023-06-27 09:45:07 +0900

    first commit

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,34 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by cw-matsuba on 2023/06/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    @StateObject private var store = ScrumStore()
+    
+    var body: some Scene {
+        WindowGroup {
+            ScrumsView(scrums: $store.scrums) {
+                Task {
+                    do {
+                        try await store.save(scrums: store.scrums)
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
+            }
+            .task {
+                do {
+                    try await store.load()
+                } catch {
+                    fatalError(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
---
+++ Scrumdinger/Scrumdinger/Views/MeetingView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by cw-matsuba on 2023/06/20.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(
+                    secondsElapsed: scrumTimer.secondsElapsed,
+                    secondsRemaining: scrumTimer.secondsRemaining,
+                    theme: scrum.theme
+                )
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
+            }
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .onAppear {
+            startScrum()
+        }
+        .onDisappear {
+            endScrum()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    private func startScrum() {
+        scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+        scrumTimer.startScrum()
+    }
+    
+    private func endScrum() {
+        scrumTimer.stopScrum()
+        let newHistory = History(attendees: scrum.attendees)
+        scrum.history.insert(newHistory, at: 0)
+    }
+ }
+    
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+    }
+}
